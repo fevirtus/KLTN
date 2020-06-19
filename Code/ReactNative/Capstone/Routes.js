51b14ad9a970b30c65a4bf08f6f9b45e841bfd8d
phongace <phongace98@gmail.com>
2020-06-19 10:09:34 +0700

    18/6
    done profile

--- Code/ReactNative/Capstone/Routes.js
+++ Code/ReactNative/Capstone/Routes.js
@@ -6,19 +6,20 @@ import Login from './src/components/LoginPage';
 import Home from './src/components/HomePage';
 import Profile from './src/components/ProfileGeneral';
 import PetProfile from './src/components/PetProfile';
-import Test from './src/components/TestImage';
+import Test from './src/components/Test';
 
 const Stack = createStackNavigator();
 
 export const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="PetProfile">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Test" component={Test} />
         <Stack.Screen name="Profile" component={Profile} options={{ title: 'Trang cá nhân' }} />
         <Stack.Screen name="PetProfile" component={PetProfile} options={{ title: 'Trang cá nhân pet' }} />
+
+        <Stack.Screen name="Test" component={Test} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Code/ReactNative/Capstone/src/components/PetProfile.js
+++ Code/ReactNative/Capstone/src/components/PetProfile.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import { Title, Subheading, Text } from 'react-native-paper'
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native'
+import { Title, Subheading, Text, TextInput } from 'react-native-paper'
 import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const colors = {
     orange: '#ff7315',
@@ -11,12 +12,113 @@ const colors = {
 }
 
 export default function PetProfile() {
+    const [name, setName] = useState('Charley')
+    const [breed, setBreed] = useState('Pitbull')
+    const [gender, setGender] = useState('Male')
+    const [weight, setWeight] = useState('5')
+    const [age, setAge] = useState('2')
+    const [city, setCity] = useState('HaNoi')
+
+    const [showModal, setShowModal] = useState(false)
+
+    const handleChangeName = text => {
+        setName(text)
+    }
+
+    const handleChangeBreed = text => {
+        setBreed(text)
+    }
+
+    const handleChangeGender = text => {
+        setGender(text)
+    }
+
+    const handleChangeWeight = text => {
+        setWeight(text)
+    }
+
+    const handleChangeAge = text => {
+        setAge(text)
+    }
+
+    const handleChangeCity = text => {
+        setCity(text)
+    }
+
     return (
         <View style={styles.container}>
+            <Modal 
+                visible={showModal} 
+                animationType='slide'
+                transparent={true}
+            >
+                <View style={styles.modalContent}>
+                    <View style={styles.form}>
+                        <AntDesign 
+                            style={styles.close}
+                            name="closecircleo"
+                            size={32}
+                            onPress={() => setShowModal(false)}
+                        />
+                        <View style={styles.content}>
+                            <Subheading style={styles.subheadingModal}>Edit Profile</Subheading>
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="Name"
+                                value={name}
+                                onChangeText={handleChangeName}
+                            />
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="Breed"
+                                value={breed}
+                                onChangeText={handleChangeBreed}
+                            />
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="Gender"
+                                value={gender}
+                                onChangeText={handleChangeGender}
+                            />
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="Weight"
+                                value={weight}
+                                onChangeText={handleChangeWeight}
+                            />
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="Age"
+                                value={age}
+                                onChangeText={handleChangeAge}
+                            />
+                            <TextInput 
+                                style={styles.textInput}
+                                mode="outlined"
+                                label="City"
+                                value={city}
+                                onChangeText={handleChangeCity}
+                            />
+                            <TouchableOpacity style={styles.saveButton} onPress={() => setShowModal(false)}>
+                                <Text style={styles.textButton}>Save</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>  
+                </View>
+            </Modal>
+
             <View style={styles.header}>
                 <Text style={styles.title}>MY PETPROFILE</Text>
                 <Image source={require('../../images/users/2.jpg')} style={styles.img}/>
-                <TouchableOpacity style={styles.buttonEdit}>
+                <TouchableOpacity 
+                    style={styles.buttonEdit} 
+                    onPress={() => setShowModal(true)}
+                >
                     <Entypo name="edit" size={25} color="white" />
                 </TouchableOpacity>
             </View>
@@ -29,29 +131,29 @@ export default function PetProfile() {
                     <View style={styles.contentLeft}>
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>Name</Subheading>
-                            <Text style={styles.text}>Charley</Text>
+                            <Text style={styles.text}>{name}</Text>
                         </View>
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>Breed</Subheading>
-                            <Text style={styles.text}>Pitbull</Text>
+                            <Text style={styles.text}>{breed}</Text>
                         </View>
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>Gender</Subheading>
-                            <Text style={styles.text}>Male</Text>
+                            <Text style={styles.text}>{gender}</Text>
                         </View>  
                     </View>
                     <View style={styles.contentRight}>
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>Weight</Subheading>
-                            <Text style={styles.text}>5 kg</Text>
+                            <Text style={styles.text}>{weight} kg</Text>
                         </View> 
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>Age</Subheading>
-                            <Text style={styles.text}>2</Text>
+                            <Text style={styles.text}>{age}</Text>
                         </View> 
                         <View style={styles.item}>
                             <Subheading style={styles.subheading}>City</Subheading>
-                            <Text style={styles.text}>Ha Noi</Text>
+                            <Text style={styles.text}>{city}</Text>
                         </View> 
                     </View>
                 </View>
@@ -64,6 +166,54 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
+    //Modal
+    modalContent: {
+        backgroundColor: '#000000aa',
+        flex: 1
+    },
+    form: {
+        backgroundColor: '#fff',
+        flex: 1,
+        borderRadius: 15,
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 40,
+        marginRight: 40,
+        paddingTop: 40,
+        paddingBottom: 40,
+        paddingLeft: 40,
+        paddingRight: 40
+    },
+    close: {
+        alignSelf: 'flex-end'
+    },
+    content: {
+        paddingTop: 20
+    },
+    subheadingModal: {
+        fontSize: 22,
+        fontWeight: '700',
+        paddingBottom: 15
+    },
+    textInput: {
+        paddingBottom: 5
+    },
+    saveButton: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    textButton: {
+        fontSize: 18,
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 5,
+        marginRight: 5
+    },
+    //Content
     header: {
         flex: 1,
         backgroundColor: colors.pink,
