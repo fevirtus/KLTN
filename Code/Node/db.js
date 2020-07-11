212026944fd5bb279a57619f88de36842dbfe73d
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-11 22:05:09 +0700

    [ANHTV] add api documentation

--- Code/Node/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('pet_dating_test', 'root', 'Passmysql4869', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
