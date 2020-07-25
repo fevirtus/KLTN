a1c184251f71b0a7fedd6529dedc0b7f7fb74472
VAnhTrinh4869 <trinhvanh4869@gmail.com>
2020-07-25 22:42:23 +0700

    show all users but current user chat screen

--- Code/ReactNative/Capstone/src/container/HomePage/Chat/index.js
+++ Code/ReactNative/Capstone/src/container/HomePage/Chat/index.js
@@ -1,19 +1,123 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native'
-import { Container } from '../../../components'
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, FlatList } from 'react-native'
+import ShowUsers from '../../../components/ShowUsers';
+import database from '@react-native-firebase/database';
+import { useSelector } from 'react-redux';
 
-const Chat = () => {
+const Chat = ({ navigation }) => {
+
+    const [allUsers, setAllUsers] = useState([]);
+    const userInfo = useSelector(state => state.auth.userInfo);
+
+    useEffect(() => {
+        try {
+            database().ref('users').on('value', dataSnapshot => {
+                // let users = dataSnapshot.map(child => {
+                //     return {
+                //         id: child.val().uuid,
+                //         name: child.val().name,
+                //         profileImg: child.val().profileImg,
+                //     }
+                // })
+                let users = []
+                dataSnapshot.forEach((child) => {
+                    if (userInfo.uid != child.val().uuid) {
+                        users.push({
+                            id: child.val().uuid,
+                            name: child.val().name,
+                            profileImg: child.val().profileImg,
+                        });
+                    }
+
+                });
+                setAllUsers(users);
+            })
+        } catch (error) {
+            alert(error)
+        }
+    }, [])
+
+    // * ON IMAGE TAP
+    const imgTap = (profileImg, name) => {
+        // if (!profileImg) {
+        //     navigation.navigate("ShowFullImg", {
+        //         name,
+        //         imgText: name.charAt(0),
+        //     });
+        // } else {
+        //     navigation.navigate("ShowFullImg", { name, img: profileImg });
+        // }
+    };
+
+    // * ON NAME TAP
+    const nameTap = (profileImg, name, guestUserId) => {
+        // if (!profileImg) {
+        //     navigation.navigate("Chat", {
+        //         name,
+        //         imgText: name.charAt(0),
+        //         guestUserId,
+        //         currentUserId: uuid,
+        //     });
+        // } else {
+        //     navigation.navigate("Chat", {
+        //         name,
+        //         img: profileImg,
+        //         guestUserId,
+        //         currentUserId: uuid,
+        //     });
+        // }
+    };
+
     return (
-        <View>
-            <Container>
-                <Text>Chat nhaaa</Text>
-            </Container>
-        </View>
+        <SafeAreaView style={{ flex: 1, }}>
+            {/* {getScrollPosition > getOpacity() && (
+        <StickyHeader
+          name={name}
+          img={profileImg}
+          onImgTap={() => imgTap(profileImg, name)}
+        />
+      )} */}
+
+            {/* ALL USERS */}
+            <FlatList
+                alwaysBounceVertical={false}
+                data={allUsers}
+                keyExtractor={(_, index) => index.toString()}
+                // onScroll={(event) =>
+                //     setScrollPosition(event.nativeEvent.contentOffset.y)
+                // }
+                // ListHeaderComponent={
+                //   <View
+                //     style={{
+                //       opacity:
+                //         getScrollPosition < getOpacity()
+                //           ? (getOpacity() - getScrollPosition) / 100
+                //           : 0,
+                //     }}
+                //   >
+                //     <Profile
+                //       img={profileImg}
+                //       onImgTap={() => imgTap(profileImg, name)}
+                //       onEditImgTap={() => selectPhotoTapped()}
+                //       name={name}
+                //     />
+                //   </View>
+                // }
+                renderItem={({ item }) => (
+                    <ShowUsers
+                        name={item.name}
+                        img={item.profileImg}
+                        onImgTap={() => imgTap(item.profileImg, item.name)}
+                        onNameTap={() => nameTap(item.profileImg, item.name, item.id)}
+                    />
+                )}
+            />
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
 
 export default Chat
--- Code/ReactNative/Capstone/src/utility/colors/index.js
+++ Code/ReactNative/Capstone/src/utility/colors/index.js
@@ -14,3 +14,7 @@ export const PINK = "#EE66AA";
 export const ORANGE = "#EEAA66";
 export const LIGHT_GRAY = '#dedcee';
 export const LIGHT_PINK = '#ffe8df';
+
+export const SEMI_TRANSPARENT = "rgba(0,0,0,0.5)";
+export const DARK_GRAY = "rgb(46, 46, 46)";
+export const SILVER = "rgb(192,192,192)";
