88230813507904d5f0a5576a7c2bbd5b007e9ef1
Khuong <40598527+khuongnda1998@users.noreply.github.com>
2020-08-14 23:24:55 +0700

    khuongnda update

--- Database/version1/BD diagram pet dating.sql
+++ Database/version1/BD diagram pet dating.sql
@@ -1,28 +1,41 @@
 CREATE TABLE `User` (
-  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int AUTO_INCREMENT,
   `user_name` nvarchar(255),
+  `uid_login` nvarchar(max),
   `user_dob` Datetime,
   `user_gender` boolean,
-  `user_status` int,
+  `user_email` nvarchar(max),
+  `user_address` nvarchar(255),
   `user_phone` nvarchar(255),
   `user_avatar` nvarchar(max),
-  `privacy` nvarchar(255),
+  `privacy` int,
+  `is_block` int,
+  `date_block` Datetime,
   `created_at` Datetime,
   `last_update` Datetime,
-  `local_id` int
+  `local_id` int,
+  PRIMARY KEY (`user_id`, `uid_login`)
+);
+
+CREATE TABLE `User_Vip` (
+  `user_id` int,
+  `status` int,
+  `confirm_img` nvarchar(max),
+  `upload_at` Datetime,
+  `date_start` Datetime
 );
 
 CREATE TABLE `Location` (
   `location_id` int PRIMARY KEY AUTO_INCREMENT,
-  `local_status` int,
-  `local_name` nvarchar(255),
+  `local_latitude` double,
+  `local_longitude` double,
   `last_update` Datetime
 );
 
 CREATE TABLE `Account` (
   `account_id` int PRIMARY KEY AUTO_INCREMENT,
-  `role` int,
-  `account_status` int,
+  `user_name` nvarchar(255),
+  `password` nvarchar(255),
   `last_update` Datetime
 );
 
@@ -30,14 +43,14 @@ CREATE TABLE `Conversation` (
   `con_id` int PRIMARY KEY AUTO_INCREMENT,
   `user_one` int,
   `user_two` int,
-  `create_at` Datetime default time_stamp(),
+  `create_at` Datetime,
   `last_update` Datetime
 );
 
-CREATE TABLE `Followed` (
-  `follow_id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_follow` int,
-  `user_taget` int
+CREATE TABLE `Feedback` (
+  `feedback_id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_feedback` int,
+  `content` vachar(max)
 );
 
 CREATE TABLE `Multimedia_Storage` (
@@ -51,11 +64,13 @@ CREATE TABLE `Multimedia_Storage` (
 CREATE TABLE `Pet` (
   `pet_id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int,
-  `pd_id` int,
+  `pb_id` int,
   `pet_dob` Datetime,
   `pet_name` nvarchar(255),
   `pet_gender` nvarchar(255),
   `pet_avatar` nvarchar(max),
+  `weight` double,
+  `is_active` boolean,
   `pet_status` int,
   `introduction` nvarchar(max),
   `create_at` Datetime,
@@ -95,24 +110,26 @@ CREATE TABLE `Pet_next_generation` (
 CREATE TABLE `Pet_reaction` (
   `reaction_id` int PRIMARY KEY AUTO_INCREMENT,
   `pet_id` int,
-  `user_id` int
+  `user_id` int,
+  `reaction_time` Datetime,
+  `reaction_status` int
 );
 
 ALTER TABLE `User` ADD FOREIGN KEY (`local_id`) REFERENCES `Location` (`location_id`);
+
+ALTER TABLE `User_Vip` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
 
 ALTER TABLE `Conversation` ADD FOREIGN KEY (`user_one`) REFERENCES `User` (`user_id`);
 
 ALTER TABLE `Conversation` ADD FOREIGN KEY (`user_two`) REFERENCES `User` (`user_id`);
 
-ALTER TABLE `Followed` ADD FOREIGN KEY (`user_follow`) REFERENCES `User` (`user_id`);
-
-ALTER TABLE `Followed` ADD FOREIGN KEY (`user_taget`) REFERENCES `User` (`user_id`);
+ALTER TABLE `Feedback` ADD FOREIGN KEY (`user_feedback`) REFERENCES `User` (`user_id`);
 
 ALTER TABLE `Multimedia_Storage` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
 
 ALTER TABLE `Pet` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
 
-ALTER TABLE `Pet` ADD FOREIGN KEY (`pd_id`) REFERENCES `Pet_Breed` (`pet_breed_id`);
+ALTER TABLE `Pet` ADD FOREIGN KEY (`pb_id`) REFERENCES `Pet_Breed` (`pet_breed_id`);
 
 ALTER TABLE `Pet_Feature` ADD FOREIGN KEY (`pet_id`) REFERENCES `Pet` (`pet_id`);
 
@@ -125,3 +142,4 @@ ALTER TABLE `Pet_match` ADD FOREIGN KEY (`p_next_generation`) REFERENCES `Pet_ne
 ALTER TABLE `Pet_reaction` ADD FOREIGN KEY (`pet_id`) REFERENCES `Pet` (`pet_id`);
 
 ALTER TABLE `Pet_reaction` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
+
